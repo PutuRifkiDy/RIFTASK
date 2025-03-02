dfff20ba3142a0b4344358d6fe35807dfe6f5b07
PutuRifkiDy <puturifki56@gmail.com>
2025-03-02 20:36:32 +0800

    add(components): get prioriy badge, get status badge, and header

---
+++ resources/js/Components/GetPriorityBadge.jsx
@@ -0,0 +1,35 @@
+import { PRIORITY } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
+
+export default function GetPriorityBadge({ priority }) {
+    const { URGENT, HIGHT, MEDIUM, LOW, UKNOWN } = PRIORITY;
+
+    let badge, text;
+
+    switch (priority) {
+        case URGENT:
+            badge = 'bg-red-500 hover:bg-red-600';
+            text = URGENT;
+            break;
+        case HIGHT:
+            badge = 'bg-yellow-500 hover:bg-yellow-600';
+            text = HIGHT;
+            break;
+        case MEDIUM:
+            badge = 'bg-blue-500 hover:bg-blue-600';
+            text = MEDIUM;
+            break;
+        case LOW:
+            badge = 'bg-green-500 hover:bg-green-600';
+            text = LOW;
+            break;
+        default:
+            badge = '';
+            text = UKNOWN;
+    }
+
+    return(
+        <Badge className={badge}>{text}</Badge>
+    );
+
+}
