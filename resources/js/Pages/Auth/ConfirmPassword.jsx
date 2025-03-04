3770238cc21c81802853f7151d3b2e8410a90622
PutuRifkiDy <puturifki56@gmail.com>
2025-03-04 21:58:56 +0800

    Update title di Confirm Password

--- resources/js/Pages/Auth/ConfirmPassword.jsx
+++ resources/js/Pages/Auth/ConfirmPassword.jsx
@@ -29,9 +29,9 @@ export default function ConfirmPassword() {
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <Card>
-                        <CardHeader>
+                        <CardHeader className="flex flex-col items-center justify-center">
                             <Link href="/" className="text-4xl font-black leading-relaxed tracking-tighter">
-                                Plannify<span className="text-red-500">.</span>
+                                RifsTasks<span className="text-red-500">.</span>
                             </Link>
                             <h2 className="text-left text-lg font-medium leading-9 tracking-tight text-muted-foreground">
                                 This is a secure area of the application. Please confirm your password before
