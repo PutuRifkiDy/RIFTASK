68991f30ea24533eeb5799ce8c13931876376201
PutuRifkiDy <puturifki56@gmail.com>
2025-03-04 23:23:52 +0800

    Mengganti Layot sebelumnya menjadi persistance layout agar dirender sekali

--- resources/js/Pages/Testing.jsx
+++ resources/js/Pages/Testing.jsx
@@ -1,5 +1,7 @@
 import AppLayout from '@/Layouts/AppLayout';
 
 export default function Testing() {
-    return <AppLayout>this is testing</AppLayout>;
+    return <div>this is testing</div>;
 }
+
+Testing.layout = (page) => <AppLayout children={page} title="Testing" />;
