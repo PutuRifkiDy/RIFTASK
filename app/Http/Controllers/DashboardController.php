860910e29027635923c1587c2941eb37fcf2f2de
PutuRifkiDy <puturifki56@gmail.com>
2025-03-09 18:33:20 +0800

    Menampilkan ChartProductivity

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use App\Models\Card;
 use App\Models\Member;
 use App\Models\User;
 use App\Models\Workspace;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Inertia\Response;
 
@@ -14,11 +15,12 @@ class DashboardController extends Controller
     public function index(): Response
     {
         return inertia('Dashboard', props: [
-            'page_settings' => [
+            'page_settings'      => [
                 'title'    => 'Dashboard',
                 'subtitle' => 'Welcome to your dashboard',
             ],
-            'count'         => [
+            'productivity_chart' => $this->productivityChart(),
+            'count'              => [
                 'users'      => fn()      => User::count(),
                 'workspaces' => fn() => Member::query()
                     ->where('members.user_id', request()->user()->id)
@@ -38,5 +40,93 @@ class DashboardController extends Controller
                     ->count(),
             ],
         ]);
+    }
+
+    public function productivityChart(): array
+    {
+        $currentDate  = Carbon::now();
+        $sixMonthsAgo = $currentDate->copy()->addMonths(-5);
+
+        $labels   = [];
+        $datasets = [
+            [
+                'label'           => 'To Do',
+                'data'            => [],
+                'backgroundColor' => 'rgba(239, 68 , 68, 1)',
+            ],
+            [
+                'label'           => 'In Progress',
+                'data'            => [],
+                'backgroundColor' => 'rgba(59, 130, 246, 1)',
+            ],
+            [
+                'label'           => 'On Review',
+                'data'            => [],
+                'backgroundColor' => 'rgba(250, 204 , 21, 1)',
+            ],
+            [
+                'label'           => 'Dones',
+                'data'            => [],
+                'backgroundColor' => 'rgba(34, 197 , 94, 1)',
+            ],
+        ];
+
+        for ($i = 0; $i < 6; $i++) {
+            $month    = $sixMonthsAgo->format('F');
+            $labels[] = $month;
+
+            $taskCountTodo = Member::query()
+                ->where('members.user_id', request()->user()->id)
+                ->whereHasMorph(
+                    'memberable',
+                    Card::class,
+                    fn($query) => $query->where('status', CardStatus::TODO->value),
+                )
+                ->whereMonth('created_at', $sixMonthsAgo->month)
+                ->count();
+
+            $taskCountInProgress = Member::query()
+                ->where('members.user_id', request()->user()->id)
+                ->whereHasMorph(
+                    'memberable',
+                    Card::class,
+                    fn($query) => $query->where('status', CardStatus::INPROGRESS->value),
+                )
+                ->whereMonth('created_at', $sixMonthsAgo->month)
+                ->count();
+
+            $taskCountOnReview = Member::query()
+                ->where('members.user_id', request()->user()->id)
+                ->whereHasMorph(
+                    'memberable',
+                    Card::class,
+                    fn($query) => $query->where('status', CardStatus::ONREVIEW->value),
+                )
+                ->whereMonth('created_at', $sixMonthsAgo->month)
+                ->count();
+
+            $taskCountDone = Member::query()
+                ->where('members.user_id', request()->user()->id)
+                ->whereHasMorph(
+                    'memberable',
+                    Card::class,
+                    fn($query) => $query->where('status', CardStatus::DONE->value),
+                )
+                ->whereMonth('created_at', $sixMonthsAgo->month)
+                ->count();
+
+            $datasets[0]['data'][] = $taskCountTodo;
+            $datasets[1]['data'][] = $taskCountInProgress;
+            $datasets[2]['data'][] = $taskCountOnReview;
+            $datasets[3]['data'][] = $taskCountDone;
+
+            $sixMonthsAgo->addMonth();
+        }
+
+        return [
+            'label' => $labels,
+            'datasets' => $datasets,
+        ];
+
     }
 }
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -2,10 +2,38 @@ import Header from '@/Components/Header';
 import Widget from '@/Components/Widget';
 import AppLayout from '@/Layouts/AppLayout';
 import { usePage } from '@inertiajs/react';
+import { BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 import { PiBriefcase, PiCheckLight, PiSquaresFour, PiUserCheck } from 'react-icons/pi';
 
-export default function Dashboard({count, page_settings}) {
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+export default function Dashboard({count, page_settings, productivity_chart}) {
     const auth = usePage().props.auth.user;
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Productivity Chart',
+            },
+        }
+    };
+
+    const data = {
+        labels: productivity_chart.label,
+        datasets: productivity_chart.datasets.map((dataset) => (
+            {
+                label: dataset.label,
+                data: dataset.data,
+                backgroundColor: dataset.backgroundColor
+            }
+        )),
+    };
     return (
         <>
             <div>
@@ -20,10 +48,13 @@ export default function Dashboard({count, page_settings}) {
                                 <Widget icon={<PiCheckLight className='w-6 h-6 text-white' />} title="Dones" count={count.tasks} bgColor='bg-green-400'/>
                             </dl>
                         </div>
+                        <div className='px-4 pt-5 pb-6 mt-8 bg-white border rounded-lg sm:px-6 sm:pt-6'>
+                            <Bar options={options} data={data} />
+                        </div>
                     </div>
                 </div>
             </div>
-            <aside className='fixed inset-y-0 right-0 hidden px-4 py-6 overflow-y-auto border-l border-gray-200 w-96 lg:px-8 xl:block'>
+            <aside className='fixed inset-y-0 right-0 hidden w-96 px-4 py-6 overflow-y-auto border-l border-gray-200 lg:px-8 xl:block'>
                 {/* card */}
             </aside>
         </>
